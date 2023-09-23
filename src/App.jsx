9552d27fde2e5ee187c24594621694f38804a845
gitxfu <76028008+gitxfu@users.noreply.github.com>
2023-09-23 12:51:13 -0400

    Checkpoint 5

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,23 @@ const [multiplier, setMultiplier] = useState(1);
 
 const updateCount = () => setCount(count + multiplier);
 
-
-
 const buyDoubleStuffed = () => {
   if (count >= 10) {
+    setCount(count-10);
     setMultiplier(multiplier * 2);
   }
 }
 
 const buyPartyPack = () => {
   if (count >= 100) {
+    setCount(count-100);
     setMultiplier(multiplier * 5);
   }
 }
 
 const buyFullFeast = () => {
   if (count >= 1000) {
+    setCount(count-1000);
     setMultiplier(multiplier * 10);
   }
 }
@@ -48,12 +49,12 @@ return (
       <div className="upgrade">
         <h3>Party Pack 🎉</h3>
         <p>5x per click</p>
-        <button>100 samosas</button>
+        <button onClick={buyPartyPack}>100 samosas</button>
       </div>
       <div className="upgrade">
         <h3>Full Feast 👩🏽‍🍳 </h3>
         <p>10x per click</p>
-        <button>1000 samosas</button>
+        <button onClick={buyFullFeast}>1000 samosas</button>
       </div>
     </div>
 
