5e1a70943237a970f72c346818366d6395c4448a
gitxfu <76028008+gitxfu@users.noreply.github.com>
2023-09-23 02:17:54 -0400

    Checkpoint 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
+import { useState } from 'react';
 
 const App = () => {
+  
+
+const [count, setCount] = useState(0);
+const [multiplier, setMultiplier] = useState(1);
+
+const updateCount = () => setCount(count + multiplier);
+
+
 
   return (
     <div className="App">
       <div className="header">
         <h1>Samosa Selector</h1>
-        <h2> Count: </h2>
-        <img src="/samosa.png" className="samosa"/>
+        <h2> Count: {count} </h2>
+        <img src="/samosa.png" className="samosa" onClick={updateCount} />
       </div>
     </div>
   )
